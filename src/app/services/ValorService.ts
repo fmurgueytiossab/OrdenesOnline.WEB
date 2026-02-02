7c52f9d3ad4df347d47c14f3824d6e52a13062c8
Fernando Manuel <fmurgueytio@seminariosab.com.pe>
2026-02-02 11:47:37 -0500

    cambio leve
    Se agreggo el verdadero url del " api " valor

--- src/app/services/ValorService.ts
+++ src/app/services/ValorService.ts
@@ -9,7 +9,7 @@ import { Observable } from 'rxjs';
 })
 export class ValorService {
 
-  private apiUrl = 'https://10.80.1.15/api';
+  private apiUrl = 'https://10.80.1.15/api/valor';
   //private apiUrl = 'https://localhost:7213/api/valor';
 
   constructor(private http: HttpClient) {}
